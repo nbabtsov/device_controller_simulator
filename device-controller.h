d9bd33eaec3c2ff6721ff14156dd2094350df664
nbabtsov <31867318+nbabtsov@users.noreply.github.com>
2023-09-01 15:47:31 -0700

    Update device-controller.h

--- device-controller.h
+++ device-controller.h
@@ -1,11 +1,11 @@
 #ifndef DEVICE_CONTROLLER_H
 #define DEVICE_CONTROLLER_H
 
-/* You must implement this function.
+/* 
  * The device controller is informing you 1 byte has been read.
  * c contains the byte */
 void read_interrupt(int c);
-/* You must implement this function.
+/* 
  * The device controller is informing you 1 byte has been written, as requested. */
 void write_done_interrupt();
 
